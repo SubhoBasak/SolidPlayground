2750076b94e94f32a32082ba9eb3d80ce5cb936b
SubhoBasak <subhobasak50@gmail.com>
2023-09-18 22:40:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Link, Route, Routes } from "@solidjs/router";
-import { lazy } from "solid-js";
+import { Suspense, lazy } from "solid-js";
 
 function App() {
   return (
@@ -10,21 +10,23 @@ function App() {
         <Link href="/contact">Contact</Link>
         <Link href="/gallery">Gallery</Link>
       </nav>
-      <Routes>
-        <Route path="/" component={lazy(() => import("@lake/pages/home"))} />
-        <Route
-          path="/about"
-          component={lazy(() => import("@lake/pages/about"))}
-        />
-        <Route
-          path="/contact"
-          component={lazy(() => import("@lake/pages/contact"))}
-        />
-        <Route
-          path="/gallery"
-          component={lazy(() => import("@lake/pages/gallery"))}
-        />
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" component={lazy(() => import("@lake/pages/home"))} />
+          <Route
+            path="/about"
+            component={lazy(() => import("@lake/pages/about"))}
+          />
+          <Route
+            path="/contact"
+            component={lazy(() => import("@lake/pages/contact"))}
+          />
+          <Route
+            path="/gallery"
+            component={lazy(() => import("@lake/pages/gallery"))}
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 }
